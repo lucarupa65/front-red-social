f1f1d1aede1a8a60fd4bf155ebc6661ec60efa4c
Luis Carlos <lucarupa65@gmail.com>
2024-02-04 11:41:37 -0500

    Diseno

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { AddComponent } from '../../components/feed/add/add.component';
+import { FeedService } from '../../services/feed.service';
 
 @Component({
   selector: 'app-home',
@@ -16,8 +17,10 @@ import { AddComponent } from '../../components/feed/add/add.component';
 })
 export class HomeComponent { 
   private authService = inject(AuthService);
+  private feedServise = inject(FeedService);
 
   public currentUser = computed(() => this.authService.currentUser());
+  public feeds = computed(() => this.feedServise.feedAll());
   public hidden: boolean = true;
 
 
